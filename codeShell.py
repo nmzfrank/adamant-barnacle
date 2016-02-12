005e6db0531ee3a49943d3dec54e7a66d1a9ad03
nmzfrank <nmzfrank@sjtu.edu.cn>
2016-02-13 00:29:32 +0800

    try menu and toplevel

--- codeShell.py
+++ codeShell.py
@@ -1,28 +1,47 @@
 import Encode
 import Tkinter as Tk
+import tkMessageBox
 
 
 def encode(rstr,txtbox):
 	rg = Encode.randomGenerator(0.724,1000)
 	plaintext = rstr;
-	e = Encode.encode(plaintext,rg)
-	ciphertext = e.transform()
-	txtbox.insert(0,ciphertext)
+	try:
+		e = Encode.encode(plaintext,rg)
+		ciphertext = e.transform()
+		txtbox.delete(0,Tk.END)
+		txtbox.insert(0,ciphertext)
+	except Encode.rangeException:
+		tkMessageBox.showinfo(message="empty entry!")
 
 
+def customSetting(win,g_setting):
+	print 1
+	top = Tk.Toplevel(win)
+	top.title("settings...")
 
 
 def main():
+	g_setting = []
 	win = Tk.Tk()
 	win.title('codeShell')
 	win.geometry('800x200')
-	str1 = Tk.Entry(win)
+
+	menubar = Tk.Menu(win)
+	settingmenu = Tk.Menu(menubar,tearoff=0)
+	settingmenu.add_command(label="custom settings...",command= lambda: customSetting(win,g_setting))
+
+	menubar.add_cascade(label="setting",menu=settingmenu,)
+
+	str1 = Tk.Entry(win,width= 50)
 	str2 = Tk.Entry(win)
-	btn1 = Tk.Button(win,text="||\n\\/",command= lambda: encode(str1.get(),str2))
+	btn = Tk.Button(win,text="||\n\\/",command= lambda: encode(str1.get(),str2))
 	
 	str1.pack()
 	btn.pack()
 	str2.pack()
+	
+	win.config(menu=menubar)
 	Tk.mainloop()
 
 
