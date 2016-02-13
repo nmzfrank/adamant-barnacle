0e8c6d70c8ddda9a79f79e3507a86a6fba932b5b
nmzfrank <nmzfrank@sjtu.edu.cn>
2016-02-13 21:49:06 +0800

    replace entry with text add scroll bar add load from file

--- codeShell.py
+++ codeShell.py
@@ -1,46 +1,76 @@
 import Encode
 import Tkinter as Tk
 import tkMessageBox
+import tkFileDialog
 
-
-def encode(rstr,txtbox):
-	rg = Encode.randomGenerator(0.724,1000)
-	plaintext = rstr;
+def encode(rstr,txtbox,g_setting):
+	rg = Encode.randomGenerator(g_setting['randomSeed'],g_setting['randomStart'])
+	plaintext = rstr[:-1];
 	try:
 		e = Encode.encode(plaintext,rg)
 		ciphertext = e.transform()
-		txtbox.delete(0,Tk.END)
-		txtbox.insert(0,ciphertext)
+		txtbox.delete(1.0,Tk.END)
+		txtbox.insert(1.0,ciphertext)
 	except Encode.rangeException:
 		tkMessageBox.showinfo(message="empty entry!")
 
+def loadFile(win,str1):
+	 filename = tkFileDialog.askopenfilename(initialdir = 'D:')
+	 if(filename):
+	 	fp = open(filename)
+	 	str1.insert(1.0, fp.read()) 
+
+
+
 
 def customSetting(win,g_setting):
-	print 1
 	top = Tk.Toplevel(win)
 	top.title("settings...")
 
 
+
+
 def main():
-	g_setting = []
+	g_setting = {'randomSeed':0.724,'randomStart':1000}
 	win = Tk.Tk()
 	win.title('codeShell')
-	win.geometry('800x200')
+	win.geometry('600x400')
+
+	
+
+	inputframe = Tk.Frame(win)
+	outputframe = Tk.Frame(win)
+	
+	scroll1 = Tk.Scrollbar(inputframe, orient=Tk.VERTICAL)
+	str1 = Tk.Text(inputframe, height=5 , yscrollcommand = scroll1.set)
+	scroll1.config(command=str1.yview)
+
+	scroll2 = Tk.Scrollbar(outputframe, orient=Tk.VERTICAL)
+	str2 = Tk.Text(outputframe, height=5, yscrollcommand=scroll2.set)
+	scroll2.config(command=str2.yview)
+
+	btn = Tk.Button(win,text="||\n\\/",command= lambda: encode(str1.get(1.0, Tk.END),str2,g_setting))
+	
+	inputframe.pack()
+	scroll1.pack(side=Tk.RIGHT,fill=Tk.Y)
+	str1.pack(side=Tk.LEFT)
+	btn.pack()
+	outputframe.pack()
+	scroll2.pack(side=Tk.RIGHT,fill=Tk.Y)
+	str2.pack(side=Tk.LEFT)
+
 
 	menubar = Tk.Menu(win)
+
+	filemenu = Tk.Menu(menubar,tearoff=0)
+	filemenu.add_command(label="load from files...", command=lambda: loadFile(win,str1))
+	menubar.add_cascade(label="file",menu=filemenu)
+
 	settingmenu = Tk.Menu(menubar,tearoff=0)
 	settingmenu.add_command(label="custom settings...",command= lambda: customSetting(win,g_setting))
+	menubar.add_cascade(label="setting",menu=settingmenu)
 
-	menubar.add_cascade(label="setting",menu=settingmenu,)
 
-	str1 = Tk.Entry(win,width= 50)
-	str2 = Tk.Entry(win)
-	btn = Tk.Button(win,text="||\n\\/",command= lambda: encode(str1.get(),str2))
-	
-	str1.pack()
-	btn.pack()
-	str2.pack()
-	
 	win.config(menu=menubar)
 	Tk.mainloop()
 
